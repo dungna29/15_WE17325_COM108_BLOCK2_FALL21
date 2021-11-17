ab39e1df13b8413298a97e35c074edd2616f44e2
Dungna89 <dungna29@fpt.edu.vn>
2021-11-17 20:31:17 +0700

    1

--- 15_WE17325_COM108_BLOCK2_FALL21/15_WE17325_COM108_BLOCK2_FALL21/Main.c
+++
@@ -1,5 +0,0 @@
-﻿#include <stdio.h>
-int main()
-{
-	//Học bài đầu tiên tạo project
-}
---
+++ 15_WE17325_COM108_BLOCK2_FALL21/Bai_1.0_GioiThieuLapTrinhC/Main.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+/*
+ *  Có 2 Kiểu người học lập trình:
+ *		+ Người code
+ *		+ Người chép code
+ */
+ /*
+  * Làm quen cơ bản với lập trình C
+  * 1. #include <stdio.h> import thư viện C vào
+  * 2. Một chương trình phải có 1 hàm main và chỉ có 1
+  * 3. Một câu lệnh phải kết thúc bằng ;
+  * 4. Ctrl + F5: Chạy chương trình
+  * 5. Ctrl + Space: Dùng để gợi ý code
+  * 6. Ctrl + D: Nhân bản code
+  *	7. Ctrl + K + D: Format code - Clean Code
+  *	8. Ctrl + K + C: Comment code
+  *	9. Ctrl + K + U: Mở Comment
+  */
+
+int main()//Hàm main và có ký hiệu () gọi là hàm
+{//Mở hàm
+	printf("Toi muon hoc lap trinh \n");//; dùng để kết thúc 1 câu lệnh
+
+	//Bên trong 1 hàm thì gọi là Body code
+
+	printf("Toi khong muon chep code \n");//\n là xuống dòng
+	printf("Toi khong muon chep code \n");
+	printf("Toi khong muon chep code \n");
+	printf("Toi khong muon chep code \n");
+	printf("Toi khong muon chep code \n");
+	printf("Toi khong muon chep code \n");
+
+}//Đóng hàm
