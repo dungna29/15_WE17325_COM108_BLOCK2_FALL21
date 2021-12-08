1b087b39761d94c321ca03c7c8399ae2a5a530e4
Dungna89 <dungna29@fpt.edu.vn>
2021-12-08 19:31:09 +0700

    1

---
+++ 15_WE17325_COM108_BLOCK2_FALL21/BAI_TAP_MANG_ARRAY/Main.c
@@ -0,0 +1,17 @@
+﻿/*
+ * Bài 1: Khai báo 1 mảng số nguyên gồm các phần tử sau
+ * int arrNumberFpoly[] = {88,7,9,6,5,8};	 *
+ *  - In số 5 ra màn hình bằng câu printf.
+ *  - In tất cả các phần tử có ở trong mảng ra màn hình
+ *  - In tất cả các giá trị chẵn ra màn hình.
+ *
+ *	Bài 2: Cho người dùng nhập vào 1 mảng số nguyên và in tổng dãy số người dùng đã nhập ra màn hình.
+ *
+ *	Bài 3: Cho người dùng nhập vào 1 mảng số nguyên rồi in ra số lớn nhất trong mảng
+ *
+ *	Bài 4: Cho người dùng nhập vào 1 mảng số nguyên cho in ra các số chẵn lẻ trên 2 dòng riêng biệt.
+ *
+ *	Bài 5: Coh người dùng nhập vào 1 mảng chẵn nếu là số lẻ nhập vào bắt nhập lại.
+ *
+ *
+ */
---
+++ 15_WE17325_COM108_BLOCK2_FALL21/Bai_2.1_ThuatToanSapXepNoiBot/Main.c
@@ -0,0 +1,103 @@
+﻿#include <stdio.h>
+//Thuật toán nổi bọt - Bubble Sort
+
+void SapXepTangDanByDungna(int arr[],int doDaiCuaMang)
+{
+	for (int i = 0; i < doDaiCuaMang -1; ++i)
+	{
+		for (int j = i+1; j < doDaiCuaMang; ++j)
+		{
+			if (arr[i]> arr[j])//nếu muốn muốn sắp xếp giảm dần chỉ cần đảo dấu > thành <
+			{
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	printf_s("Mang sau khi sap xep: \n");
+	for (int i = 0; i < doDaiCuaMang; ++i)
+	{
+		printf_s("%d ", arr[i]);
+	}
+	printf_s("\n");
+}
+int main()
+{
+	int arrNumbers[] = { 9,5,4,6 };
+	printf_s("Mang chua sap xep: \n");
+	for (int i = 0; i < 4; ++i)
+	{
+		printf_s("%d ", arrNumbers[i]);
+	}
+	printf_s("\n");
+	SapXepTangDanByDungna(arrNumbers,4);
+	printf_s("=================\n");
+	int arrNumbers1[] = { 9,5,4,6,99,100,555,66,1 };
+	SapXepTangDanByDungna(arrNumbers1,9);
+	//Áp dụng thuật toán vào
+	/*
+	 * arr: 9 5 4 6
+	 * Vòng i = 0, j = 1: 9 > 5 True
+	 * 5 9 4 6
+	 * Vòng i = 0, j = 2: 5 > 4 True
+	 * 4 9 5 6
+	 * Vòng i = 0, j = 3: 4 > 6 False
+	 * 4 9 5 6
+	 *
+	 * Vòng i = 1, j = 2: 9 > 5 True
+	 * 4 5 9 6
+	 * Vòng i = 1, j = 3: 5 > 6 False
+	 * 4 5 9 6
+	 *
+	 * Vòng i = 2, j = 3: 9 > 6	True
+	 * 4 5 6 9
+	 *
+	 * Vòng i Tăng lên 3 sẽ kết thúc không chạy nữa
+	 */
+	// for (int i = 0; i < 4-1; ++i)
+	// {
+	// 	for (int j = i+1; j < 4; ++j)
+	// 	{
+	// 		if (arrNumbers[i]> arrNumbers[j])//nếu muốn muốn sắp xếp giảm dần chỉ cần đảo dấu > thành <
+	// 		{
+	// 			int temp = arrNumbers[i];
+	// 			arrNumbers[i] = arrNumbers[j];
+	// 			arrNumbers[j] = temp;
+	// 		}
+	// 	}
+	// }
+
+	// for (int i = 0; i < 4-1; i++)
+	// {
+	// 	printf("Lan chay thu: %d \n", i);
+	// 	printf("Gia Tri Tai Vi Tri %d \n", arrNumbers[i]);
+	// 	for (int j = i + 1; j < 4; j++)
+	// 	{
+	// 		int temp = arrNumbers[i];
+	// 		printf("Gia Tri cua %d > %d \n", arrNumbers[i], arrNumbers[j]);
+	// 		if (arrNumbers[i] > arrNumbers[j])
+	// 		{
+	// 			arrNumbers[i] = arrNumbers[j];
+	// 			arrNumbers[j] = temp;
+	// 		}
+	// 		for (int i = 0; i < 4; i++)
+	// 		{
+	// 			printf("%d ", arrNumbers[i]);
+	// 		}
+	// 		printf("\n");
+	// 	}
+	// 	printf("Sau khi sap xep lan thu %d \n", i);
+	// 	for (int i = 0; i < 4; i++)
+	// 	{
+	// 		printf("%d", arrNumbers[i]);
+	// 	}
+	// 	printf_s("\n");
+	// }
+	// printf_s("Mang sau khi sap xep: \n");
+	// for (int i = 0; i < 4; ++i)
+	// {
+	// 	printf_s("%d ", arrNumbers[i]);
+	// }
+	// printf_s("\n");
+}
