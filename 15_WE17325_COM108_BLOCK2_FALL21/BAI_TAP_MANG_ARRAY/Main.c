5a7650940ce89b144f36522ae35b49824ccd6900
Dungna89 <dungna29@fpt.edu.vn>
2021-12-10 20:34:12 +0700

    1

--- 15_WE17325_COM108_BLOCK2_FALL21/BAI_TAP_MANG_ARRAY/Main.c
+++ 15_WE17325_COM108_BLOCK2_FALL21/BAI_TAP_MANG_ARRAY/Main.c
@@ -1,6 +1,8 @@
-﻿/*
+﻿#include <stdio.h>
+#define size
+/*
  * Bài 1: Khai báo 1 mảng số nguyên gồm các phần tử sau
- * int arrNumberFpoly[] = {88,7,9,6,5,8};	 *
+ * int arrNumberFpoly[] = {88,7,9,6,5,8};	 
  *  - In số 5 ra màn hình bằng câu printf.
  *  - In tất cả các phần tử có ở trong mảng ra màn hình
  *  - In tất cả các giá trị chẵn ra màn hình.
@@ -11,7 +13,32 @@
  *
  *	Bài 4: Cho người dùng nhập vào 1 mảng số nguyên cho in ra các số chẵn lẻ trên 2 dòng riêng biệt.
  *
- *	Bài 5: Coh người dùng nhập vào 1 mảng chẵn nếu là số lẻ nhập vào bắt nhập lại.
+ *	Bài 5: Cho người dùng nhập vào 1 mảng chẵn nếu là số lẻ nhập vào bắt nhập lại.
+ * 
+ *  Bài 6: Nhập vào cân nặng các người yêu cũ tìm ra vị trí người có số cân nặng bé nhất.
+ *
+ *	Bài 7: Nhập vào năm sinh các người yêu cũ in ra người yêu cũ có tuổi lớn nhất và thứ tự nhập vào.
+ *	Ví dụ: Năm 2000 Tuổi 21 Người yêu thứ 3.
  *
  *
  */
+void BaiMau()
+{
+	int arrNumber[100], soLuong;
+	printf_s("Moi ban nhap so luong: ");
+	scanf_s("%d", &soLuong);
+	for (int i = 0; i < soLuong; ++i)
+	{
+		printf_s("Moi ban nhap gia tri tai Index[%d]: ", i);
+		scanf_s("%d", &arrNumber[i]);
+	}
+	for (int i = 0; i < soLuong; ++i)
+	{
+		printf_s("Index[%d] = %d \n", i, arrNumber[i]);
+	}
+}
+int main()
+{
+	
+
+}
---
+++ 15_WE17325_COM108_BLOCK2_FALL21/Bai_2.2_StringTrongC/Main.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+/*
+ * Chuỗi trong C nó là 1 mảng ký tự
+ * Java - String, C# - string (chuỗi)
+ */
+int main()
+{
+	//Nguyen Ba Ton = Chuỗi = Tập hợp của nhiều ký tự = Mảng Ký tự.
+
+	char arrName[] = { 'T','O','N' };
+	//Khai báo 1 mảng kiểu char
+	char name[100];
+	printf_s("Moi ban nhap ten: ");
+	//scanf_s("%s", name, 100); Không in được sau khoảng trắng.
+	//scanf_s("%[^\n]", name, 100);//Sử dụng Regex	
+	//fgets(name, 100, stdin);
+	gets_s(name, 100);//Sử dụng cách này khi đi thi
+	printf_s("Chao ban %s", name);
+
+	/*
+	 * Nhập các thông tin SV FPOLY sau:
+	 *	1. Tên đầy đủ
+	 *	2. Năm sinh
+	 *	3. Sdt
+	 *	4. Điểm Thi C
+	 *	Sau đó in ra màn hình như sau:
+	 *	Tên + Năm sinh + Tuổi + SDT + Diểm C - (Qua môn hoặc học lại)	 
+	 */
+}
